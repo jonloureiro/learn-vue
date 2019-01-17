68c6e8a40195bf1dd0a38bc77dadd41fb0a13bc5
Jonathan Loureiro <jonathan.loureiro.m@gmail.com>
2019-01-16 21:10:07 -0300

    Separação da folha de estilos e script do html

---
+++ main.js
@@ -0,0 +1,79 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        user: null,
+        email: '',
+        form_login: {
+            email: '',
+            senha: '',
+            conf_senha: '',
+        },
+        config: {
+            apiKey: "AIzaSyA5Z7hfhGNoTBADcusyhhzPvu5Kgnin_nc",
+            authDomain: "admin-e-users.firebaseapp.com",
+            databaseURL: "https://admin-e-users.firebaseio.com",
+            projectId: "admin-e-users",
+            storageBucket: "",
+            messagingSenderId: "451591112883"
+        },
+        fb: null
+    },
+    computed: {
+        hasUser: function() {
+            return (this.user != null);
+        }
+    },
+    methods: {
+        login: function(e) {
+            e.preventDefault();
+
+            var email = this.form_login.email;
+            var password = this.form_login.senha;
+
+            this.fb.auth()
+            .signInWithEmailAndPassword(email, password)
+            .catch(function(error) {
+                console.log(error.code);
+                console.log(error.message);
+            });
+        },
+        cadastrar: function(e) {
+            e.preventDefault();
+
+            var email = this.form_login.email;
+            var password = this.form_login.senha;
+
+            if (password != this.form_login.conf_senha) {
+                console.log('Senhas precisam ser iguais.');
+                return;
+            }
+
+            this.fb.auth()
+            .createUserWithEmailAndPassword(email, password)
+            .catch(function(error) {
+                console.log(error.code);
+                console.log(error.message);
+            });
+        },
+        logout: function() {
+            this.fb.auth()
+            .signOut()
+            .catch(function(error) {
+              console.log(error.code);
+              console.log(error.message);
+            });
+        },
+        authUser: function(user) {
+            this.user = user;
+            if (user == null) {
+                this.email = '';
+            } else {
+                this.email = user.email;
+            }
+        }
+    },
+    created: function() {
+        this.fb = firebase.initializeApp(this.config);
+        this.fb.auth().onAuthStateChanged(this.authUser);
+    }
+});
